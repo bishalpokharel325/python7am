cb8881538ad98e376b49eda4711f2bad5238b964
bishal <bishalpokharel325@gmail.com>
2020-03-07 18:35:57 +0545

    adding existing directory from my computer into git

---
+++ fifthdayloops/patterns/pattern.py
@@ -0,0 +1,21 @@
+# no=2
+# while no<100:
+#     div=2
+#     count=1
+#     while div<no:
+#         if no%div==0:
+#             count=count+1
+#         div=div+1
+#     if count==1:
+#         print(f"This is prime no: {no}")
+#     no=no+1
+
+i=1
+while i<=5:
+    j=0
+    while j<i:
+        print("*",end="")
+        j=j+1
+    print("\r")
+    i=i+1
+
