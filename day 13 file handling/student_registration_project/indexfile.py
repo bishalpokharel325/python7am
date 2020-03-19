a83ba537da09844af864c0fc9dfcd9abd3744778
bishalpokharel325 <61719179+bishalpokharel325@users.noreply.github.com>
2020-03-19 12:06:27 +0545

    Update and rename indexpage.py to indexfile.py

---
+++ day 13 file handling/student_registration_project/indexfile.py
@@ -0,0 +1,59 @@
+from register_module import register
+from login_module import login
+from csvtolistconverter import csvtolistconverter
+import os
+"""Check for record.csv and if this file do not exist then it will make a new one"""
+if not os.path.exists("record.csv"):
+    check_file=open("record.csv","w")
+    check_file.close()
+"""Change records in csv into list :  data[indexno][data1,2 or 3]"""
+
+data=csvtolistconverter("record.csv")
+totalno=len(data)
+"""Input from user whether: they want to register or login"""
+print("WELCOME!!!!!!")
+print("Choose whether you want to register, login or exit:")
+print("Enter 1 to register \nEnter 2 to login \nEnter any other key to exit program")
+choose=input("your choice:")
+if choose=="1":
+    os.system('cls')
+    print("This is Registration Page:")
+    print("-----------------------------")
+    checker=1
+    while checker!=0:
+        user_name = input("Enter user name:")
+        i = 0
+        while i < totalno:
+            if user_name == data[i][1]:
+                checker = 0
+                print("Username already exist.")
+                user_name = input("Please Enter new Username:")
+            i = i + 1
+    user_password=input("Enter Password:")
+    user_repassword=input("Confirm Password:")
+    while(user_password!=user_repassword):
+        print("Password could not be confirmed:")
+        user_password = input("Enter Password:")
+        user_repassword = input("Confirm Password:")
+    user_email = input("Enter Email:")
+    user_phoneno = input("Enter Phone no:")
+    """check if username exist or not."""
+
+
+    if checker==0:
+        register(totalno+1,user_name,user_password,user_email,user_phoneno)
+    secondchoice=input("Do you want to login? (Y or N)")
+    if secondchoice=="Y" or secondchoice=="y":
+        login()
+    else:
+        print("Username already exist. Please Enter new username.")
+if choose=="2":
+    login()
+
+
+
+
+
+
+
+
