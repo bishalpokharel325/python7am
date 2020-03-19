317ebbee4715497de1fa2b515823d264a671add6
bishalpokharel325 <61719179+bishalpokharel325@users.noreply.github.com>
2020-03-19 12:08:14 +0545

    Delete register_module.py

--- register_module.py
+++
@@ -1,8 +0,0 @@
-def register(id,username,password,email,phoneno):
-    try:
-        writer=open("record.csv","a")
-        writer.write(f"\n{id},{username},{password},{email},{phoneno}")
-        writer.close()
-    except Exception as e:
-        print(e)
-
